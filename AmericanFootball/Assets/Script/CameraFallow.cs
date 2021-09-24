e38e73ba3548aaa2837d7e8e478c4eb06d108669
EmirGaziKopar <75802906+EmirGaziKopar@users.noreply.github.com>
2021-09-24 17:44:58 +0300

    1.3

--- AmericanFootball/Assets/Script/CameraFallow.cs
+++ AmericanFootball/Assets/Script/CameraFallow.cs
@@ -6,6 +6,11 @@ public class CameraFallow : MonoBehaviour
 {
     public new Transform camera, man;
 
+
+    MouseLook mouseLook;
+
+    [SerializeField] GameObject mouseLookPointer;
+
     Vector3 yon;
 
     public float hiz = 10f;
@@ -16,9 +21,22 @@ public class CameraFallow : MonoBehaviour
 
     [SerializeField] Vector3 distance;
 
+
+    private void Start()
+    {
+        mouseLook = mouseLookPointer.GetComponent<MouseLook>();
+        float x = distance.x;
+        float y = distance.y;
+        float z = distance.z;
+    }
     private void FixedUpdate()
     {
+
+        //distance.x = mouseLook.yRot;
+
         camera.position = Vector3.Lerp(camera.position, man.position - distance, .05f);
+        
+        
         camera.LookAt(man);
     }
 }
