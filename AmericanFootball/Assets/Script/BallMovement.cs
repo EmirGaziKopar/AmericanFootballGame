ff5b4969ff3d2e41200a76e1bb4d4e7354af70f0
EmirGaziKopar <75802906+EmirGaziKopar@users.noreply.github.com>
2021-09-24 18:05:00 +0300

    1.4

--- AmericanFootball/Assets/Script/BallMovement.cs
+++ AmericanFootball/Assets/Script/BallMovement.cs
@@ -6,5 +6,28 @@ public class BallMovement : MonoBehaviour
 {
     public Transform sceneTransform;
 
-    
+    Animator anim;
+    [SerializeField] GameObject Character;
+    new Rigidbody rigidbody;
+
+    private void Start()
+    {
+        anim = Character.GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (this.anim.GetCurrentAnimatorStateInfo(0).IsName("shot"))
+        {
+            Debug.Log("not playing");
+            transform.parent = sceneTransform.transform;
+        }
+        else
+        {
+            Debug.Log("playing");
+        }
+        
+
+    }
 }
