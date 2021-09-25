adc45a37b6ec03e3776f1f5deea183a3a78e4ec0
EmirGaziKopar <75802906+EmirGaziKopar@users.noreply.github.com>
2021-09-25 14:59:52 +0300

    1.6

--- AmericanFootball/Assets/Script/BallMovement.cs
+++ AmericanFootball/Assets/Script/BallMovement.cs
@@ -6,46 +6,87 @@ public class BallMovement : MonoBehaviour
 {
     public Transform sceneTransform;
 
-
-    Transform Bodytransform;
+    // public GameObject normalCamera, ballCamera;    Topu attýktan sonra topu takip eden kamera düþüncesi düþüncesi
+       Transform Bodytransform;
     [SerializeField] float speed;
     Animator anim;
     [SerializeField] GameObject Character;
     new Rigidbody rigidbody;
-
+    bool isFlaying = false;
+    new ParticleSystem particleSystem;
+    [SerializeField] GameObject particleSystemPointer;
+    public float baslangic = 0f;
     [SerializeField] GameObject cube;
 
     private void Start()
     {
         anim = Character.GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();
-
         Bodytransform = cube.GetComponent<Transform>();
+        particleSystem = particleSystemPointer.GetComponent<ParticleSystem>();
+        var emission = particleSystem.emission;
+        emission.rateOverTime = baslangic;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
+        
         if (this.anim.GetCurrentAnimatorStateInfo(0).IsName("shot"))
-        {
+        {     
+            transform.rotation = Quaternion.Euler(0, 0, 0);
             Debug.Log("not playing");
             transform.parent = sceneTransform.transform;
             rigidbody.constraints = RigidbodyConstraints.None;
-
             rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
             transform.Translate(transform.forward * Time.deltaTime * speed);
             shot();
+            var emission = particleSystem.emission;
+            emission.rateOverTime = 500f;
         }
         else
         {
             Debug.Log("playing");
         }
         
+        
+    }
+ /*
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("stadium") || other.gameObject.CompareTag("character"))
+        {
+            isFlaying = false;
+            normalCamera.gameObject.SetActive(true);
+            ballCamera.gameObject.SetActive(false);
+
+
+        }
+        else
+        {
+
+            onFly();
+
+
+
+
+        }
+    }
+   
+    
+    void onFly()
+    {
+
+
+        normalCamera.gameObject.SetActive(false);
+        ballCamera.gameObject.SetActive(true);
+
 
     }
-
+ */
     void shot()
     {
-        Vector3 a = new Vector3(Bodytransform.forward.x, Random.Range(0.001f, 0.002f), Bodytransform.forward.z); //Topun karsiya gitmesini saglayan z.
+        Vector3 a = new Vector3(Bodytransform.forward.x, 0.5f,  Bodytransform.forward.z); //Topun karsiya gitmesini saglayan z.
+        
         rigidbody.velocity = a * speed;
     }
 }
